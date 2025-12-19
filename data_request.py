2359cbbcd668ad56774366aa4ef3bb4889ab2475
NoahB6JS <noah.js.b6@gmail.com>
2025-12-19 17:30:41 +0000

    using html instead of pygame for more customisation

--- data_request.py
+++ data_request.py
@@ -1,9 +1,34 @@
+
+
 import requests
+import time
 
+STOCK_LIST = ["AAPL","MSFT","GOOGL","AMZN","META","TSLA","NVDA","NFLX","AMD","INTC","ORCL","IBM","JPM","BAC","GS","V","MA","KO","PEP","MCD","NKE"]
 API_KEY = "LVEW5D8G2Z97836S"
-URL = (f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={API_KEY}")
 
-response = requests.get(URL)
+results = []
 
-data = response.json()
-print(data)
+for stock in STOCK_LIST:
+    response = requests.get(
+        "https://www.alphavantage.co/query",
+        params={
+            "function": "GLOBAL_QUOTE",
+            "symbol": stock,
+            "apikey": API_KEY
+        }
+    )
+
+    data = response.json()
+
+    if "Global Quote" in data and data["Global Quote"]:
+        price = float(data["Global Quote"]["05. price"])
+        results.append({"symbol": stock, "price": price})
+        print(f"{stock}: ${price}")
+    else:
+        print(f"{stock}: API error")
+
+    time.sleep(12) 
+
+print("\nFinal list:")
+print(results)
+
